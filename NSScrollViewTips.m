024fbd9b367679a47911555c230279c335eea105
hanxiaoqing19910916 <190658047@qq.com>
2016-12-20 18:42:56 +0800

    tijiao

---
+++ NSScrollViewTips.m
@@ -0,0 +1,35 @@
+
+
+1.背景色
+self.scrollView.drawsBackground = NO;
+self.scrollView.backgroundColor= [NSColor purpleColor];
+
+设置drawsBackground = NO 背景色无效（默认是YES）
+
+2.为scrollView的contentView增加额外的滚动区域
+self.scrollView.contentView.automaticallyAdjustsContentInsets = NO;
+self.scrollView.contentView.contentInsets = NSEdgeInsetsMake(40, 0, 40, 0);
+
+3.控制鼠标滚轮滑动量
+@property CGFloat horizontalLineScroll;
+@property CGFloat verticalLineScroll;
+@property CGFloat lineScroll;
+lineScroll同时设置水平和数值滚动量
+
+4.放大scrollView的contentView的内容
+self.scrollView.allowsMagnification = YES;
+self.scrollView.magnification = 2;
+
+设置最小/最大放大倍数
+@property CGFloat maxMagnification;
+@property CGFloat minMagnification;
+
+// 制定rect矩形区域放大
+- (void)magnifyToFitRect:(NSRect)rect;
+// 围绕某个中心点放大
+- (void)setMagnification:(CGFloat)magnification centeredAtPoint:(NSPoint)point;
+
+
+5.禁止滚动
+[_textScrollView setVerticalLineScroll:0.0];
+[_textScrollView setVerticalPageScroll:0.0];
---
+++ NSWindow官方文档翻译.m
@@ -0,0 +1,5 @@
+1.window怎么运行的
+The NSWindow class defines objects that manage and coordinate the windows an application displays on the screen. A single NSWindow object corresponds to at most one onscreen window. The two principal functions of an NSWindow object are to provide an area in which NSView objects can be placed and to accept and distribute, to the appropriate views, events the user instigates through actions with the mouse and keyboard. Note that the term window sometimes refers to the Application Kit object and sometimes to the window server’s display device; which meaning is intended is made clear in context. AppKit also defines an abstract subclass of NSWindow—NSPanel—that adds behavior more appropriate for auxiliary windows.
+
+
+ NSWindow class类
