5249b8b8551a6782035f83a87c1191d2cea3ee64
hanxiaoqing <hanxiaoqing@zhangyuhedeiMac.local>
2017-03-23 10:16:30 +0800

    提交数据结构练习demo初始化项目

---
+++ DataStructuresPractice/DataStructuresPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataStructuresPractice
+//
+//  Created by hanxiaoqing on 2017/3/23.
+//  Copyright © 2017年 BabelTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataStructuresPractice/DataStructuresPractice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DataStructuresPractice
+//
+//  Created by hanxiaoqing on 2017/3/23.
+//  Copyright © 2017年 BabelTime. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
