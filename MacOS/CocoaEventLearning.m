1dc084d5c01280f3af08b3d3f4cc25cb7d681f73
hanxiaoqing <hanxiaoqing@zhangyuhedeiMac.local>
2017-05-17 10:04:58 +0800

    提交

--- MacOS/CocoaEventLearning.m
+++ MacOS/CocoaEventLearning.m
@@ -1,4 +1,5 @@
-/**
+/**  (摘自Event Architecture)
+ 
  鼠标和触控板事件的传递路径
  
  一个NSWindow对象里面的sendEvent:方法将鼠标事件转发到涉及用户操作的视图.它调用NSView的
@@ -25,5 +26,32 @@
  （mouse-moved）事件会频繁的产生并且会阻塞事件队列，一个view必须明确的请求它的所属window去监
  听它的，通过window一个setAcceptsMouseMovedEvents:方法。定义在Other Event Dispatching
  文档说明中的追踪视图矩形区域的方法，是跟随鼠标移动位置的一个相对节省性能的方式。
+ 
+ 基于NSResponder对象的鼠标事件的方法的实现，子类化NSView可把一个鼠标事件具体化为执行一个动作响应，
+ 例如发送一个target-action消息，选取一个图形元素，对自己在特定位置进行重绘等等。每个事件方法
+ 都包含它唯一的 NSEvent对象参数，包含这个view的关于这个事件的信息，比如可以用view
+ 的locationInWindow方法找到鼠标指针基于事件接受者window坐标系的点。把它转换到view的坐标系统
+ ，可以使用 convertPoint:fromView: 传一个 nil view 参数.然后你可以用 mouse:inRect:方法
+ 决定是否要在制定的区域产生点击事件。
+ 
+ 触控板事件采用的传递路径到视图view的机制与鼠标事件类似。NSWindow对象代表触控板事件出现的窗口，
+ 转发事件到位于鼠标指针下的视图，然而存在两种触控板事件，接触类事件，和点触类事件。前者通常是原生的
+ 触控板事件（NSTabletProximity）发生在触笔进入或者离开触控板。点触类事件出现在proximity-
+ entering（进入）and proximity-leaving（进入）事件之间，象征着触笔方向，压力，按钮点击之类的
+ 事情。点触类事件常作为鼠标事件的子类。查阅Handling Tablet Events的更详细的信息
+ 
+ 
+ 
+ The Path of Key Events（键盘事件的传递路径）
+ 
+ 以下是对键盘事件传递路径详细描述，按照应用对每个键盘事件的评估的顺序。
+ 1.Key equivalents 是一个按键或者组合
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  */
