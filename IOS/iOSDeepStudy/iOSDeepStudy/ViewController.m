51f01ce2ee76754e2f1f4f5c51941f2a15ddf8a3
hanxiaoqing <hanxiaoqing@zhangyuhedeiMac.local>
2017-06-26 17:34:26 +0800

    tijiao

--- IOS/iOSDeepStudy/iOSDeepStudy/ViewController.m
+++ IOS/iOSDeepStudy/iOSDeepStudy/ViewController.m
@@ -9,19 +9,42 @@
 #import "ViewController.h"
 #import "KVCExample.h"
 
+#import "Immessage.pbobjc.h" //模型
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
+- (void)kvcTest
+{
     KVCExample *model = [[KVCExample alloc] init];
     [model setValue:@"wo" forKey:@"name"];
     [model printName];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UserInfo *uinfo = [[UserInfo  alloc] init];
+    uinfo.name = @"wang";
+    uinfo.level = 3;
+    
+    NSData *da =[uinfo delimitedData];
+    
+    
+    GPBCodedInputStream *inputStream = [GPBCodedInputStream streamWithData:da];
+    
+    NSError *error;
+    UserInfo *per = [UserInfo parseDelimitedFromCodedInputStream:inputStream extensionRegistry:nil error:&error];
+    
+    if (error){
+        return;
+    }
+
+    NSLog(@"--%@",per.name);
+    
 }
 
 
