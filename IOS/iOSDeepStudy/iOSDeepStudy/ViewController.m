c4431e724d4e520cc386862ae720e4f07d478036
hanxiaoqing <han@github.com>
2018-02-26 10:47:38 +0800

    delete

--- IOS/iOSDeepStudy/iOSDeepStudy/ViewController.m
+++ IOS/iOSDeepStudy/iOSDeepStudy/ViewController.m
@@ -9,8 +9,6 @@
 #import "ViewController.h"
 #import "KVCExample.h"
 
-#import "Immessage.pbobjc.h" //模型
-#import "VendorALL.h"
 #import "ClassToolAll.h"
 #import "HXConst.h"
 
@@ -29,54 +27,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    UserInfo *uinfo = [[UserInfo  alloc] init];
-    uinfo.name = @"wang";
-    uinfo.level = 3;
-    
-    NSData *da =[uinfo delimitedData];
-    
-    
-    GPBCodedInputStream *inputStream = [GPBCodedInputStream streamWithData:da];
-    
-    NSError *error;
-    UserInfo *per = [UserInfo parseDelimitedFromCodedInputStream:inputStream extensionRegistry:nil error:&error];
-    
-    if (error){
-        return;
-    }
-
-    NSLog(@"--%@",per.name);
-    
-    
-
-    
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
 
-    [NSString validateEmail:@"da"];
-    
-    HXApiManager *mag = [HXApiManager manager];
-    mag.urlStirng = @"http://www.baidu.com";
-    NSDictionary *d = @{@"a":@"b"};
-    [mag loadGETwithService:nil params:d success:^(HXResponsResult *response) {
-        
-
-        
-        
-    } fail:^(HXResponsResult *response) {
-        NSLog(@"---%@",response.resError);
-    }];
-    
 
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
